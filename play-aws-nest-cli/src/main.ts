93597f0e0d3e8af01ec52dee385f8d30a910c349
Andrew Miller <andrew.patrick.miller.2021@gmail.com>
2023-08-07 13:41:18 -0600

    📅  | Mon Aug  7 13:41:18 MDT 2023

--- play-aws-nest-cli/src/main.ts
+++ play-aws-nest-cli/src/main.ts
@@ -1,6 +1,5 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { AppService } from './app.service';
 import { CommandPromptService } from './command-prompt.service';
 
 async function bootstrap() {
