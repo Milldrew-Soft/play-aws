ecf60fd6d5a22aec4c3eb7f21f6bf9042be45c9d
Andrew Miller <andrew.patrick.miller.2021@gmail.com>
2023-08-07 13:31:58 -0600

    📅  | Mon Aug  7 13:31:58 MDT 2023

--- play-aws-nest-cli/src/app.module.ts
+++ play-aws-nest-cli/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CommandPromptService } from './command-prompt.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, CommandPromptService],
 })
 export class AppModule {}
